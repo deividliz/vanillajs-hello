e76e9f1d29a192fa2e7d2e3737ad6c121514412f
deividliz <lizarte41@gmail.com>
2024-11-18 14:57:47 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "my phone ", "the car "];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function generadorExcusas(lista) {
+    return lista[Math.floor(Math.random() * lista.length)];
+  }
+
+  let excusa =
+    generadorExcusas(who) +
+    generadorExcusas(action) +
+    generadorExcusas(what) +
+    generadorExcusas(when);
+
+  document.querySelector("#excusa").innerHTML = excusa;
 };
